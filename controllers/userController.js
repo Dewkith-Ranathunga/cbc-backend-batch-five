9a3b57028ce313e8683bc6a406a6b97d11250802
Dewkith <dewkithrashmika@gmail.com>
2025-05-13 20:24:55 +0530

    ✨ user controller added

---
+++ controllers/userController.js
@@ -0,0 +1,95 @@
+import bctypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
+export function createUser(req, res) {
+  if (req.body.role == "admin") {
+    if (req.user != null) {
+      if (req.user.role !== "admin") {
+        res.status(403).json({
+          message: "You are not authorized to create an admin user",
+        });
+        return;
+      }
+    } else {
+      res.status(403).json({
+        message: "You are not authorized to create an admin user. Please login first",
+      });
+      return;
+    }
+  }
+
+  const hashedPassword = bctypt.hashSync(req.body.password, 10);
+
+  const user = new User({
+    email: req.body.email,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    password: hashedPassword,
+    role: req.body.role,
+  });
+
+  user.save()
+    .then(() => {
+      res.json({
+        message: "User saved",
+      });
+    })
+    .catch(() => {
+      res.json({
+        message: "Error saving user",
+      });
+    });
+}
+
+export function loginUser(req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // "user" represents the document retrieved from the "users" collection in the database
+  // If a user with the specified email exists, "user" will contain the user document
+  // If no user is found, "user" will be null
+  User.findOne({ email: email }).then((user) => {
+    if (user == null) {
+      res.status(404).json({
+        message: "User not found",
+      });
+    } else {
+      // Compare the password with the hashed password
+      const isPasswordValid = bctypt.compareSync(password, user.password);
+      if (isPasswordValid) {
+        const token = jwt.sign(
+          {
+            email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            role: user.role,
+            img: user.img,
+          },
+          "Dew2003kith"
+        );
+
+        res.json({
+          message: "Login successful",
+          token: token,
+        });
+      } else {
+        res.status(401).json({
+          message: "Invalid password",
+        });
+      }
+    }
+  });
+}
+
+//testing commit
+
+export function isAdmin(req){
+    if (req.user == null) {
+      return false;
+    }
+    if (req.user.role !== "admin") {
+      return false;
+  }
+  return true;
+}
---
+++ index.js
@@ -0,0 +1,63 @@
+import bodyParser from "body-parser";
+import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import orderRouter from "./routes/orderRouter.js";
+import productRouter from "./routes/productRouter.js";
+import userRouter from "./routes/userRouter.js";
+
+
+
+const app = express();
+
+// Middleware to parse incoming JSON requests
+// This middleware allows the server to understand JSON data sent in requests
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  const tokenString = req.headers["authorization"]; 
+
+  if (tokenString != null) {
+    const token = tokenString.replace("Bearer ", ""); 
+    
+    jwt.verify(token, "Dew2003kith", (err, decoded) => {
+      if (decoded != null) {
+        req.user = decoded;
+        next();
+      } else {
+        console.log("invalid token");
+        res.status(403).json({
+          message: "Invalid token",
+        });
+      }
+    });
+
+  } else {
+    next();
+  }
+});
+
+
+
+
+mongoose.connect(
+    "mongodb+srv://admin:123@cluster0.xjb2uho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB");
+    console.error(err);
+    
+  });
+
+app.use("/product", productRouter);
+app.use("/user", userRouter);
+app.use("/order", orderRouter);
+
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
